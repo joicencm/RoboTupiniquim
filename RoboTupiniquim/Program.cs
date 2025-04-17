b63c9b521b221df08b2c13e8666c68892ecee511
joicencm <joicencm@gmail.com>
2025-04-16 23:16:57 -0300

    Refatoração Metodo ExibirArea com a PrimeiraPosicao e Implementação Mover para Direita e Esquerda

--- RoboTupiniquim/Program.cs
+++ RoboTupiniquim/Program.cs
@@ -6,10 +6,43 @@
         static char[,] area = new char[tamanho, tamanho];
         static void Main(string[] args)
         {
-            
-
             ExibirArea();
+            int linha = 0;
+            int coluna = 0;
+            area[linha, coluna] = '>';
+
+            while (true)
+            {
+                Console.Clear();
+                ExibirPrimeriraPosicaoRobo();
+
+                Console.WriteLine("\nDigite a direção D (Direite), E (Esquerda) ou S (Sair):");
+                string comando = Console.ReadLine().ToUpper();
+                MoverRoboDireiraEsqueda(ref linha, ref coluna, comando);
+
+                if (comando == "S")
+                    break;
+
+            }
+
             Console.ReadLine();
+
+        }
+
+        private static int MoverRoboDireiraEsqueda(ref int linha,  ref int coluna, string comando)
+        {
+                        area[linha, coluna] = '.';
+
+            if (comando == "D" && coluna < tamanho - 1)
+                coluna++;
+            else if (comando == "E" && coluna > 0)
+                coluna--;
+
+            if (comando == "E")
+                area[linha, coluna] = '<';
+            else
+                area[linha, coluna] = '>';
+            return coluna;
         }
 
         private static void ExibirArea()
@@ -21,8 +54,10 @@
 
             }
 
-            area[0, 0] = '>';
+        }
 
+        private static void ExibirPrimeriraPosicaoRobo()
+        {
             for (int l = 0; l < tamanho; l++)
             {
                 for (int c = 0; c < tamanho; c++)
