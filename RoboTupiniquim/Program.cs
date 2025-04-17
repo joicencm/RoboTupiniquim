dc7cc48d537f41ed34c2a42a0af0960f047961e3
joicencm <joicencm@gmail.com>
2025-04-16 22:09:09 -0300

    Refatoração - Criação Metodo ExibirArea

--- RoboTupiniquim/Program.cs
+++ RoboTupiniquim/Program.cs
@@ -2,28 +2,34 @@
 {
     internal class Program
     {
+        static int tamanho = 5;
+        static char[,] area = new char[tamanho, tamanho];
         static void Main(string[] args)
         {
-            int tamanho = 5;
-            char[,] area = new char[tamanho, tamanho];
+            
 
+            ExibirArea();
+            Console.ReadLine();
+        }
+
+        private static void ExibirArea()
+        {
             for (int l = 0; l < tamanho; l++)
             {
-                for( int c = 0; c < tamanho; c++)
-                area[l, c] = '.';
+                for (int c = 0; c < tamanho; c++)
+                    area[l, c] = '.';
 
             }
 
             area[0, 0] = '>';
 
-            for(int l = 0; l <tamanho; l++)
+            for (int l = 0; l < tamanho; l++)
             {
                 for (int c = 0; c < tamanho; c++)
                     Console.Write(area[l, c] + " ");
 
                 Console.WriteLine();
             }
-            Console.ReadLine();
         }
     }
 }
