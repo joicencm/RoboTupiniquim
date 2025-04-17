277041835d172db90bc6b3f15bb3314a129aa16a
joicencm <joicencm@gmail.com>
2025-04-17 20:01:50 -0300

    Implementada Estrutura Sequencial

--- RoboTupiniquim/Program.cs
+++ RoboTupiniquim/Program.cs
@@ -2,112 +2,65 @@
 {
     internal class Program
     {
-        static int tamanho = 5;
-        static char[,] area = new char[tamanho, tamanho];
-        // Direções possíveis em sentido horário: N, L, S, O
-        static char[] direcoes = { 'N', 'L', 'S', 'O' };
-        static int direcaoAtual = 0;
-
-        // Posição inicial do robô
-        static int x = 0;
-        static int y = 0;
         static void Main(string[] args)
         {
-            InicializarArea();
-            
-            while (true)
+            string posicaoIncicial = "3 3 L";
+            string comando = "MMDMMDMDDM";
+
+            string[] coordenadasIniciais = posicaoIncicial.Split(' ');
+
+            int posicaoX = Convert.ToInt32(coordenadasIniciais[0]); 
+            int posicaoY = Convert.ToInt32(coordenadasIniciais[1]);
+            char direcao = Convert.ToChar(coordenadasIniciais[2]);
+
+            char[] instrucoes = comando.ToCharArray();
+
+            for(int i = 0; i <instrucoes.Length; i++)
             {
-                Console.Clear();
-                AtualizarArea();
-                ExibirArea();
-                Console.WriteLine($"\nPosição atual: ({x},{y},{direcoes[direcaoAtual]})");
-                Console.WriteLine("Digite os comandos (E, D, M) ou S para sair:");
-                string comandos = Console.ReadLine().ToUpper();
+                char instrucoesAtual = instrucoes[i];
 
-                if (comandos == "S")
-                    break;
-
-                ExecutarComandos(comandos);
-
-            }
-
-            Console.ReadLine();
-
-        }
-
-        private static void ExecutarComandos(string comandos)
-        {
-            for (int i = 0; i < comandos.Length; i++)
-
-            {
-                char c = comandos[i];
-                if (c == 'E')
-                    direcaoAtual = (direcaoAtual + 3) % 4; //girar a esquerda
-
-                else if (c == 'D')
-                    direcaoAtual = (direcaoAtual + 1) % 4; // girar a direita
-
-                else if (c == 'M')
+                if (instrucoesAtual == 'E')
                 {
-                    MoverParaFrente();
+                    if (direcao == 'N')
+                        direcao = 'O';
+                    else if (direcao == 'O')
+                        direcao = 'S';
+                    else if (direcao == 'S')
+                        direcao = 'l';
+                    else if (direcao == 'L')
+                        direcao = 'N';
                 }
 
+                if (instrucoesAtual == 'D')
+                {
+                    if (direcao == 'N')
+                        direcao = 'L';
+                    else if (direcao == 'L')
+                        direcao = 'S';
+                    else if (direcao == 'S')
+                        direcao = 'O';
+                    else if (direcao == 'O')
+                        direcao = 'N';
+                    
+                   
+                }
 
+                else if (instrucoesAtual == 'M')
+                {
+                    if (direcao == 'N')
+                        posicaoY++;
+                    else if (direcao == 'S')
+                        posicaoY--;
+                    else if (direcao == 'O')
+                        posicaoX--;
+                    else if (direcao == 'L')
+                        posicaoX++;
+                }
             }
+
+            Console.WriteLine($"Posição final do Robô: {posicaoX} {posicaoY} {direcao}");
+            Console.ReadLine();
         }
 
-        private static void MoverParaFrente()
-        {
-            char direcao = direcoes[direcaoAtual];
-
-            if (direcao== 'N' && y < tamanho -1)
-                y++; // Norte: sobe no grid (aumenta Y)
-
-            else if (direcao == 'S' && y > 0)
-                y--; // Sul: desce no grid (diminui Y)
-            else if (direcao == 'L' && x < tamanho-1)
-                x++; // Leste: direita
-            else if (direcao == 'O' && x > 0)
-                x--; // Oeste: esquerda
-        }
-
-        private static void InicializarArea()
-        {
-            for (int l = 0; l < tamanho; l++)
-            {
-                for (int c = 0; c < tamanho; c++)
-                    area[l, c] = '.';
-            }
-        }
-
-        private static void ExibirArea()
-        {
-            for (int linha = tamanho - 1; linha >= 0; linha--)
-            {
-                for (int coluna = 0; coluna < tamanho; coluna++)
-                    Console.Write(area[linha, coluna] + " ");
-
-                Console.WriteLine();
-            }
-        }
-
-        private static void AtualizarArea()
-        {
-            InicializarArea();
-            area[y, x] = ObterSimboloDirecao();
-        }
-        static char ObterSimboloDirecao()
-        {
-            if (direcoes[direcaoAtual] == 'N')
-                return '↑';
-            else if (direcoes[direcaoAtual] == 'S')
-                return '↓';
-            else if (direcoes[direcaoAtual] == 'L')
-                return '→';
-            else if (direcoes[direcaoAtual] == 'O')
-                return '←';
-            else
-                return '?';
-        }
     }
 }
