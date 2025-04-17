23b3da257ada739900b37e8405363d6e8f0ba5f5
joicencm <joicencm@gmail.com>
2025-04-16 21:55:28 -0300

    Add project files.

---
+++ RoboTupiniquim/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RoboTupiniquim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanho = 5;
+            char[,] area = new char[tamanho, tamanho];
+
+            for (int l = 0; l < tamanho; l++)
+            {
+                for( int c = 0; c < tamanho; c++)
+                area[l, c] = '.';
+
+            }
+
+            area[0, 0] = '>';
+
+            for(int l = 0; l <tamanho; l++)
+            {
+                for (int c = 0; c < tamanho; c++)
+                    Console.Write(area[l, c] + " ");
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
